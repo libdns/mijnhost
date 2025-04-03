3a2096450427edd5cc5f68121c464ccb9579c099
zjean <zjean@users.noreply.github.com>
2025-04-03 17:26:20 +0000

    wip

--- provider.go
+++ provider.go
@@ -58,7 +58,7 @@ func (p *Provider) AppendRecords(ctx context.Context, zone string, records []lib
 	zone = normalizeZone(zone)
 	var created []libdns.Record
 	for _, record := range records {
-		result, err := p.createRecord(ctx, zone, record)
+		result, err := p.updateRecord(ctx, zone, record)
 		if err != nil {
 			return nil, err
 		}
@@ -96,20 +96,13 @@ func (p *Provider) SetRecords(ctx context.Context, zone string, records []libdns
 	var results []libdns.Record
 	var resultErr error
 	for _, libRecord := range records {
-		exists := isRecordExists(zoneRecords, libRecord)
-		if exists {
-			record, err := p.updateRecord(ctx, zone, libRecord)
-			if err != nil {
-				resultErr = err
-			}
-			results = append(results, record.libDNSRecord(zone))
-		} else {
-			record, err := p.createRecord(ctx, zone, libRecord)
-			if err != nil {
-				resultErr = err
-			}
-			results = append(results, record.libDNSRecord(zone))
+	
+		record, err := p.updateRecord(ctx, zone, libRecord)
+		if err != nil {
+			resultErr = err
 		}
+		results = append(results, record.libDNSRecord(zone))
+		
 	}
 
 	return results, resultErr
